88cedf6d2ccb4bbe9ecaf6bbe312d7d9c5cc2e12
VanHeezy <VanHeezy@users.noreply.github.com>
2016-07-14 17:16:29 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,66 @@
+var bio={
+	"name" : "Stephen Vanover",
+	"role" : "Software Developer & Ninja",
+	"phone" : "210-275-7656",
+	"email" : "stephenvanover1@gmail.com",
+	"location" : "Plano, Texas",
+	"bioPic" : "",
+	"backGroundMessage" : "Welcome to my Resume! Feel free to look around and reach out through my contact information."
+};
+
+console.log(bio.name);
+
+var work={
+	"jobs": [
+	{
+		"employer" : "USAA",
+		"position" : "Software Developer",
+		"dates" : "August 2014 - Present",
+		"location" : "Plano, Texas",
+		"description" : "Worked with innovative and cutting edge teams. Our teams focused on innovating our scrum and agile processes. We worked with Java and Javascript to create a rewards redemption service for our membership. We also transferred existing Wicket code over to the React.js framework. "
+	},
+	{
+		"employer" : "US Army",
+		"position" : "Team Leader",
+		"dates" : "Jan 2006 - Jan 2011",
+		"location" : "Killeen, Texas",
+		"description" : "Killing. More to come on this later."
+	}
+	]
+};
+
+var education={
+	"schools": [
+	{
+	"school" : "Oklahoma Panhandle State University",
+	"degree" : "Bachelor of Business Administration",
+	"schoolDates" : "January 2011 - Jan 2013",
+	"location" : "Goodwell, Oklahoma",
+	"major" : "Computer Information Systems"
+	},
+	{
+	"school" : "University of Texas - Dallas",
+	"degree" : "Masters of Computer Science",
+	"schoolDates" : "January 2017 - Present",
+	"location" : "Dallas, Texas"
+	}
+]
+};
+
+var projects={
+
+	"pastProjects": [
+	{
+		"projectTitle" : "Portfolio",
+		"projectDates" : "April 2015",
+		"projectDescription" : "Portfolio of stuff with some stuff I did.",
+		"projectImage" : "http://www.cdc.gov/animalimportation/images/dog2.jpg"
+	},
+	{
+		"projectTitle" : "Resume",
+		"projectDates" : "May 2015",
+		"projectDescription" : "This thing on this website place...Ya.",
+		"projectImage" : "https://aliceinreaderland.files.wordpress.com/2013/01/grumpy-cat.png?w=250&h=200&crop=1"
+	}
+]
+};
